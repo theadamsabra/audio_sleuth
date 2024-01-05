0cbfdd3c75196447d88fba5ef6c6930c1892e968
theadamsabra <theadamsabra@gmail.com>
2024-01-05 18:00:17 +0000

    clean up tests

--- tests/test_resample.py
+++ tests/test_resample.py
@@ -1,5 +1,5 @@
 import torch
-from audio_sleuth.augmentations import ResampleBlock
+from audio_sleuth.augmentations import Resample 
 
 VECTOR = torch.randn(441000)
 LABELS = torch.randn(441000)
@@ -7,12 +7,12 @@ INPUT_SR = 44100
 NEW_SR = 32000
 
 def test_resample_to_from_sr():
-    RESAMPLE = ResampleBlock(INPUT_SR, NEW_SR, return_original_sr=True)
+    RESAMPLE = Resample(INPUT_SR, NEW_SR, return_original_sr=True)
     vector, labels = RESAMPLE(VECTOR, LABELS)
     assert vector.shape == labels.shape, 'Arrays are not the same shape after resampling and returning to original sampling rate.'
 
 
 def test_resample_to_sr():
-    RESAMPLE = ResampleBlock(INPUT_SR, NEW_SR, return_original_sr=False)
+    RESAMPLE = Resample(INPUT_SR, NEW_SR, return_original_sr=False)
     vector, labels = RESAMPLE(VECTOR, LABELS)
     assert vector.shape == labels.shape, 'Arrays are not the same shape after resampling and returning to original sampling rate.'
