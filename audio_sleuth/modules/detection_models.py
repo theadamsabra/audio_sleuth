426a37c117fb22bec46369124028c0b93e720eab
theadamsabra <theadamsabra@gmail.com>
2024-01-09 02:20:11 +0000

    first working detection model!

--- audio_sleuth/modules/detection_models.py
+++ audio_sleuth/modules/detection_models.py
@@ -1,18 +1,29 @@
 import torch
 import torch.nn as nn
 from torch import Tensor
-from transformers import Wav2Vec2ForPreTraining 
+from transformers import AutoConfig
+from transformers.models.wav2vec2.modeling_wav2vec2 import (
+    Wav2Vec2PreTrainedModel,
+    Wav2Vec2Model
+)
 
 class Wav2Vec2FramewiseDetection(nn.Module):
-    def __init__(self, wav2vec2_huggingface_id:str, wav2vec2_dimensionality:int, \
-                num_classes:int, activation:nn.Module, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.feature_extractor = Wav2Vec2ForPreTraining.from_pretrained(wav2vec2_huggingface_id)
-        assert num_classes > 1, "How are you predicting less than two classes? num_classes greater than 1."
-        self.linear = nn.Linear(in_features=wav2vec2_dimensionality, out_features=num_classes)   
-        self.activation = activation 
+    def __init__(self, wav2vec2_huggingface_id:str, num_classes:int) -> None:
+        super().__init__()
+        self.config = AutoConfig.from_pretrained(
+            wav2vec2_huggingface_id,
+            num_labels=num_classes
+        )
+        self.feature_extractor = Wav2Vec2Model(self.config)
+        self.dense = nn.Linear(self.config.hidden_size, self.config.hidden_size)
+        self.dropout = nn.Dropout(self.config.final_dropout)
+        self.out_proj = nn.Linear(self.config.hidden_size, self.config.num_labels)
 
     def forward(self, waveform:Tensor) -> Tensor:
-        features = self.feature_extractor(waveform)
-        output = self.activation(self.linear(features.projected_states))
-        return output 
+        x = self.feature_extractor(waveform)[0]
+        x = self.dropout(x)
+        x = self.dense(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+        x = self.out_proj(x)
+        return x 
