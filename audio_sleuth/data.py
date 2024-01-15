07f06f4b5949ecf1a6b9f525d9a30058f76e22fa
theadamsabra <theadamsabra@gmail.com>
2024-01-15 19:31:21 +0000

    add actual collate fn

--- audio_sleuth/data.py
+++ audio_sleuth/data.py
@@ -4,8 +4,31 @@ import librosa
 import math
 from torch.nn import Module
 from torch import Tensor
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
+'''
+Collate function below:
+'''
+def pad_and_transform_collate(data_loader:DataLoader, transform:Module=None) -> tuple[Tensor, Tensor, list, list]:
+    # Setup all necessary output params
+    processed_audio = []
+    final_labels = []
+
+    # Loop through batch and run transforms:
+    for audio, labels in data_loader:
+        # Run transform if defined:
+        if transform:
+           audio, labels = transform(audio, labels)
+        processed_audio.append(audio) 
+        final_labels.append(labels)
+    
+    processed_audio = torch.nn.utils.rnn.pad_sequence(processed_audio, batch_first=True).unsqueeze(1).transpose(2, 3)
+    final_labels = torch.nn.utils.rnn.pad_sequence(final_labels, batch_first=True)
+    return processed_audio, final_labels.long()
+
+'''
+Datasets below:
+'''
 class HalfTruthDataset(Dataset):
     '''
     Torch dataset of Half Truth Dataset by Jiangyan Yi, Ye Bai, Jianhua Tao, Haoxin Ma, Zhengkun Tian, 
