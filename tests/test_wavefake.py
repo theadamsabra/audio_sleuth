20e4f91c2fbc97f0bb0ad76f4c2040d225d53bdd
theadamsabra <theadamsabra@gmail.com>
2024-02-13 02:44:50 +0000

    never enough tests

--- tests/test_wavefake.py
+++ tests/test_wavefake.py
@@ -3,7 +3,8 @@ from audio_sleuth.data.datasets import WaveFake, HalfTruthDataset
 ROOT_DIR = 'audio_data/wavefake'
 FS = 16000
 
-def test_wavefake():
+def test_wavefake_with_only_generated_data():
     wavefake = WaveFake(ROOT_DIR, FS)
     audio, labels = wavefake[0]
     assert audio.shape == labels.shape
+    assert labels[0].item() == 1
